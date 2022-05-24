afed4f963cd6544f7c9446fcc5d68c80e8b7b357
TKHTS <sakuranohanabira1269@gmail.com>
2022-05-24 13:03:48 -0700

    Add function to create new skill list

--- app/Http/Controllers/SkillController.php
+++ app/Http/Controllers/SkillController.php
@@ -4,12 +4,32 @@ namespace App\Http\Controllers;
 use App\Models\Skill;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use Illuminate\Validation\Rule;
 
 class SkillController extends Controller
 {
     public function index(){
         return Inertia::render('Skills/All', [
             'skills' => Skill::all(),
+            'availableColors' => Skill::getAvailableBackgroundColors(),
         ]);
+    }
+
+    public function store(Request $request){
+        $request->validate([
+            'name' => [
+                'required',
+                'max:255',
+                Rule::unique(Skill::class)
+            ],
+            'color' => [
+                'required',
+                'in:' . implode(',', Skill::getAvailableBackgroundColors())
+            ],
+            ]);
+
+            Skill::create($request->all());
+
+            return redirect()->route('skills.index');
     }
 }
--- resources/js/Pages/Skills/All.vue
+++ resources/js/Pages/Skills/All.vue
@@ -1,14 +1,41 @@
 <script>
 import AppLayout from "@/Layouts/AppLayout.vue";
 import JetButton from "@/jetstream/Button";
+import JetModal from "@/Jetstream/Modal";
+import JetInput from "@/Jetstream/Input";
+import JetInputError from "@/Jetstream/InputError";
 
 export default {
   components: {
     AppLayout,
     JetButton,
+    JetInput,
+    JetModal,
+    JetInputError,
   },
   props: {
     skills: Object,
+    availableColors: Object,
+  },
+  methods: {
+    submit() {
+      this.form.submit("post", route("skills.store"), {
+        onSuccess: () => {
+          this.form.reset("name");
+          this.form.reset("color");
+          this.acting = null;
+        },
+      });
+    },
+  },
+  data() {
+    return {
+      acting: null,
+      form: this.$inertia.form({
+        name: "",
+        color: "",
+      }),
+    };
   },
 };
 </script>
@@ -20,8 +47,76 @@ export default {
     </template>
 
     <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-        <table v-if="skills.length > 0" class="w-full">
+      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 text-right">
+        <jet-button
+          class="
+            p-3
+            border-3 border-indigo-500
+            text-indigo-500
+            bg-indigo-50
+            hover:bg-indigo-200
+          "
+          @click="acting = true"
+          >Add new +</jet-button
+        >
+
+        <jet-modal :show="acting" closable="true" @close="acting = null">
+          <div class="bg-gray-50 shadow-2xl p-8">
+            <form
+              class="flex flex-col items-center p-16"
+              @submit.prevent="submit"
+            >
+              <jet-input
+                class="px-5 py-3 w-96 border border-gray-600 rouded"
+                type="text"
+                name="name"
+                v-model="form.name"
+                placeholder="Skill name"
+              >
+              </jet-input>
+              <jet-input-error :message="form.errors.name" />
+
+              <select
+                class="w-96 border border-gray-600 rounded mt-5"
+                v-model="form.color"
+              >
+                <option value="">Select a color</option>
+                <option
+                  v-for="color in availableColors"
+                  :key="color"
+                  :value="color"
+                >
+                  {{ color }}
+                </option>
+              </select>
+
+              <jet-input-error :message="form.errors.color" />
+
+              <jet-button
+                class="
+                  px-5
+                  py-3
+                  mt-5
+                  w-96
+                  bg-purple-800
+                  justify-center
+                  rounded-xl
+                  test-sm
+                "
+                :disabled="form.processing"
+              >
+                <span
+                  class="animate-spin mr-1 bg-gray-700"
+                  v-show="form.processing"
+                  >&#9696;</span
+                >
+                <span v-show="!form.processing">Register</span>
+              </jet-button>
+            </form>
+          </div>
+        </jet-modal>
+
+        <table v-if="skills.length > 0" class="w-full text-left">
           <thead class="border-b-2 border-gray-300 text-indigo-600">
             <tr>
               <th class="px-6 py-3 text-left">Name</th>
@@ -34,7 +129,8 @@ export default {
               <td class="px-6 py-4">{{ skill.name }}</td>
               <td class="px-py-4">
                 <p>
-                  <span class="mr-5 py-4 px-6 rounded" :class="skill.color"> </span>
+                  <span class="mr-5 py-4 px-6 rounded" :class="skill.color">
+                  </span>
                   {{ skill.color }}
                 </p>
               </td>
